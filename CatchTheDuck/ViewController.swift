f10e2070eb715822a22837422ecb8b5c37ab55cc
Kadir Akyol <102226254+kadirakyol@users.noreply.github.com>
2022-06-20 16:27:15 +0300

    some code's

--- CatchTheDuck/ViewController.swift
+++ CatchTheDuck/ViewController.swift
@@ -8,9 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    // Variables
     var score = 0
+    var timer = Timer()
+    var hideTimer = Timer()
+    var counter = 0
+    var duckArray = [UIImageView]()
     
+    // Views
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var highScoreLabel: UILabel!
@@ -25,11 +30,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var duck8: UIImageView!
     @IBOutlet weak var duck9: UIImageView!
     
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         scoreLabel.text = "Score: \(score)"
         
+        // images
         duck1.isUserInteractionEnabled = true
         duck2.isUserInteractionEnabled = true
         duck3.isUserInteractionEnabled = true
@@ -61,13 +69,62 @@ class ViewController: UIViewController {
         duck8.addGestureRecognizer(recognizer8)
         duck9.addGestureRecognizer(recognizer9)
         
+        // Duck Array
+        
+       duckArray = [duck1,duck2,duck3,duck4, duck5, duck6, duck7, duck8, duck9]
+        
+        
+        // Timer
+        counter = 10
+        timeLabel.text = "\(counter)" // or. String(counter)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countDown), userInfo: nil, repeats: true)
+        hideTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(hideDuck), userInfo: nil, repeats: true)
+        
+        hideDuck()
+        
+        
+    }
+    
+    @objc func hideDuck() {
+        
+        for duck in duckArray {
+            duck.isHidden = true
+        }
+        
+        let random = Int (arc4random_uniform(UInt32(duckArray.count - 1)))
+        duckArray[random].isHidden = false
+        
+        
     }
     
     @objc func increaseScore() {
         score += 1
         scoreLabel.text = "Score :\(score)"
     }
-
-
+    
+    @objc func countDown() {
+        counter -= 1
+        timeLabel.text = String(counter)
+        
+        if counter == 0 {
+            timer.invalidate()
+            hideTimer.invalidate()
+            
+            for duck in duckArray {
+                duck.isHidden = true
+            }
+            
+            // Alert
+            
+            let alert = UIAlertController(title: "Time's UP!", message: "Again?", preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
+            let replayButton = UIAlertAction(title: "Replay", style: UIAlertAction.Style.default) { UIAlertAction in
+                // Replay Function
+            }
+            
+            alert.addAction(replayButton)
+            alert.addAction(okButton)
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
 }
-
